28075195d4b0f9ba2ecf5f1a91cabfc77a3c24c0
parkashSaini <128597775+parkashSaini@users.noreply.github.com>
2023-03-22 18:48:59 +0530

    added src folder with files
    added app jsx file

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/common/Header";
+
+export default function App(){
+    return (
+       <>
+       <div className="container-fluid">
+        <div className="row">
+          <Header/>
+        </div>
+       </div>
+       </>
+    );
+}
